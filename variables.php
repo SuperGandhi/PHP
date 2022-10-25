aa87a43506f7c287d4ce209a1300729451ad1159
SuperGandhi <patrickborjabueno@hotmail.com>
2022-10-25 09:20:54 -0500

    Complete

---
+++ variables.php
@@ -0,0 +1,15 @@
+
+
+<?php
+    // variables strings
+    $string = 'Julian';
+    // variables numéricas
+    $integer = 0;
+    $float = 0.12;
+    // matrices usando constructor array
+    $numbers = array('One', 'Two' , 'Three');
+
+    $string = $integer; // también se puede asignar el contenido a otra variable
+    echo $integer;
+    echo $numbers[2]
+?>
